3e9c6db5881fc993937bd33bb8ba63cc37a303bc
Abi Evans <50892754+ace01603@users.noreply.github.com>
2024-07-25 09:02:43 +0100

    close #4 converted to Next

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <p>Child components will go here</p>
+        </main>
+    );
+}
