1118fe09ce8eb4b579961c0ee14577f76d0c05c6
Abi Evans <50892754+ace01603@users.noreply.github.com>
2024-07-30 12:39:40 +0100

    seems to be processing multiline input

--- src/app/repl.js
+++ src/app/repl.js
@@ -1,21 +1,71 @@
 import styles from "./codeOutput.module.css";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import CodeOutput from "./codeOutput";
 import Sk from "skulpt";
 import RawInput from "./rawInput";
 
+const MODE = {
+    PRIMARY: 0,
+    SECONDARY: 1
+}
+
+const PROMPT = {
+    PRIMARY: ">>> ", 
+    SECONDARY: "... "
+};
+
 const Repl = () => {
 
-    const commandEntered = command => {
-        // Execute the user's command if it doesn't contain an input
-        // const matchInputs = command.match(/\s*input\((.*)\)/)
-        // if (matchInputs === null || matchInputs.length === 0) {
-        runInteractive(command);
+
+    const modeRef = useRef([MODE.PRIMARY]);
+    const commandRef = useRef("");
+    const waitForInput = useRef(false);
+
+    const isTrigger = commandChar => commandChar === ":" || commandChar === "/";
+
+    const updateMode = command => {
+        const currentMode = modeRef.current[0];
+        const enterSecondary = command.length > 0 && isTrigger(command[command.length - 1]);
+        if (currentMode === MODE.PRIMARY) {
+            if (enterSecondary) {
+                console.log("Adding secondary to current mode stack:", modeRef.current);
+                modeRef.current.unshift(MODE.SECONDARY);
+            }
+        } else if (currentMode === MODE.SECONDARY) {
+            if (command.length === 0) {
+                console.log("Pop secondary mode");
+                modeRef.current.shift();
+            }
+        }
     }
 
-    const newPrompt = () => <RawInput key="start" prompt={">>> "} submitHandler={val => commandEntered(val)} />
+    const newPrompt = prompt => <RawInput key="start" prompt={prompt} submitHandler={val => commandEntered(val)} />
 
-    const [out, setOut] = useState([newPrompt()]);
+    const commandEntered = command => {
+        updateMode(command);
+        const currentMode = modeRef.current[0];
+        console.log("current mode", currentMode)
+        // Add the command to the commandRef
+        if (command.length > 0) {
+            if (currentMode === MODE.SECONDARY) {
+                commandRef.current = `${commandRef.current.length > 0? commandRef.current + "\n" : ""}${command}`
+            } else {
+                commandRef.current = command;
+            }
+        }
+        if (currentMode === MODE.PRIMARY) {
+            console.log("Running:")
+            console.log(commandRef.current);
+            runInteractive(commandRef.current);
+        } else {
+            addOutput([newPrompt(PROMPT.SECONDARY)]);
+        }
+        
+    }
+
+    // Need a notion of mode - primary, secondary (after : or \)...need to track with a stack to handle nesting... back out of secondary by pressing enter twice
+    const [out, setOut] = useState([newPrompt(PROMPT.PRIMARY)]);
+    
 
 
     /**
@@ -23,34 +73,40 @@ const Repl = () => {
      * @param {string[] | Element[]} items 
      */
     const addOutput = items => {
-        console.log("addOutput", items);
         setOut(prevItems => [...prevItems, ...items]);
     }
 
     const skulptOutput = item => {
-        console.log("skulptOutput", item)
+        // Phantom output caused by multiline statement execution
+        console.log("Skulpt:", item)
         setOut(prevItems => [...prevItems, item]);
     }
 
-    // VERY BROKEN!
     const inputFunc = prompt => {
-        console.log("input", prompt);
+        // console.log("input", prompt);
+        waitForInput.current = true;
         const p = new Promise(function(resolve, reject) {
             // Need to let existing input resolve first
             const userIn = <RawInput prompt={prompt} submitHandler={val => {
-                console.log("resolving input", prompt)
+                // console.log("resolving input", prompt)
+                waitForInput.current = false;
                 resolve(val);
             }} />
-            console.log("adding input");
+            // console.log("adding input");
             addOutput([userIn]);
         });
         return p;
     }
 
-    const runInteractive = userIn => {
-        console.log("running with", userIn);
+    
+    /**
+     * Attempts to run the last line in the REPL environment and print any output to the terminal
+     * @param {string} userIn The response to the last prompt
+     */
+    const runInteractive = (userIn) => {
+        // console.log("running with", userIn);
         const rawIn = userIn;
-        // Configure Skulpt to execute the code
+        const isSingleCommand = userIn.split("\n").length === 1;
         Sk.configure({
             output: skulptOutput,
             __future__: Sk.python3,
@@ -62,23 +118,20 @@ const Repl = () => {
             let outputResult = true;
             if(userIn.match(/\s*import/)) {
                 outputResult = false;
-            } else {
-                userIn = "__result = " + userIn;	
+            } else if (isSingleCommand) {
+                userIn = "__result = " + userIn;
             }
             try {
                 let r = eval(Sk.compile(userIn, "repl", "exec", true).code)(Sk.globals);
-                let startTime = new Date().getTime();
+                console.log(Sk.globals)
                 if(r.$isSuspension) {
                     if(r.data.promise) {
                         r.data.promise.then(function(result) {
                             if(outputResult) {
                                 console.log("Suspension complete", rawIn, result);
-                                
-                                // The line below causes an error... result is not what it needs - try calling runInteractive with the input replaced with result
-                                // addOutput([Sk.ffi.remapToJs(Sk.builtin.repr(result)), newPrompt()]);	
-                                // addOutput([newPrompt()]);
-
                                 const replaceInput = userIn.replace(/\s*input\(.*\)/, `"${result}"`);
+                                commandRef.current = replaceInput;
+                                console.log("After input run", commandRef.current)
                                 runInteractive(replaceInput);
                             }
                         }).catch(function (error) {
@@ -88,80 +141,29 @@ const Repl = () => {
                     } else {
                         r = r.resume();
                     }
-                    let now = new Date().getTime();
-                    if(now - startTime > 5000) {
-                        addOutput(["Stopped after 5s to prevent browser crashing", newPrompt()]);
-                        // break;
-                    }
                 } 
                 if(r.__result && outputResult) {
-                    if (r.__result.v !== null) {
-                        console.log("Run complete", rawIn);
-                        addOutput([Sk.ffi.remapToJs(Sk.builtin.repr(r.__result)), newPrompt()]);
+                    commandRef.current = "";
+                    if (r.__result.v !== null && isSingleCommand) {
+                        console.log("Run complete", rawIn, r);
+                        addOutput([Sk.ffi.remapToJs(Sk.builtin.repr(r.__result)), newPrompt(PROMPT.PRIMARY)]);
                     } else {
                         console.log("Run complete 2", rawIn);
-                        addOutput([newPrompt()]);
+                        addOutput([newPrompt(PROMPT.PRIMARY)]);
                     }
+                } else if (!waitForInput.current) {
+                    commandRef.current = "";
+                    addOutput([newPrompt(PROMPT.PRIMARY)]);
                 }
             } catch (evalError) {
+                commandRef.current = "";
                 const msgs = evalError.args.v.map(obj => "Error: " + obj.v);
                 console.log("eval", rawIn, evalError);
-                addOutput([...msgs, newPrompt()]);
+                addOutput([...msgs, newPrompt(PROMPT.PRIMARY)]);
             }
-            
         }
     }
 
-    /**
-     * Attempts to run the last line in the REPL environment and print any output to the terminal
-     * @param {string} code The response the last prompt
-     * @returns {null}
-     */
-    // const runInteractiveOLD = () => {
-    //     // UPDATING STATE ON EACH LINE WILL REFRESH THE STATE... (previous executions will be dropped)... NEED A DIFFERENT APPROACH? 
-    //     // Configure Skulpt to execute the code
-    //     Sk.configure({
-    //         output: addOutput,
-    //         __future__: Sk.python3,
-    //         inputfun: inputFunc,
-    //         inputfunTakesPrompt: true
-    //     });
-
-    //     Sk.inputfun(">>> ").then(function(result) {
-    //         console.log(result);
-    //         let outputResult = false;
-    //         if(result.length > 0) {
-    //             outputResult = true;
-    //             if(code.match(/\s*import/)) {
-    //                 outputResult = false;
-    //             } else {
-    //                 code = "__result = " + code;	
-    //             }
-    //             let r = eval(Sk.compile(code, "repl", "exec", true).code)(Sk.globals);
-    //             let startTime = new Date().getTime();
-    //             while(r.$isSuspension) {
-    //                 if(r.data.promise) {
-    //                     r.data.promise.then(function(result) {
-    //                         if(outputResult) {
-    //                             addOutput(Sk.ffi.remapToJs(Sk.builtin.repr(result)));	
-    //                         }
-    //                     }).catch(function (error) {"ERROR", console.log(error)});
-    //                 } else {
-    //                     r = r.resume();
-    //                 }
-    //                 let now = new Date().getTime();
-    //                 if(now - startTime > 5000) {
-    //                     //PythonIDE.showHint("Stopped after 5s to prevent browser crashing");
-    //                     break;
-    //                 }
-    //             } 
-    //             if(r.__result && outputResult) {
-    //                 console.log("b")
-    //                 addOutput(Sk.ffi.remapToJs(Sk.builtin.repr(r.__result)));
-    //             }
-    //         }
-    //     }).catch(function (error) {console.log(error)});
-    // }
 
 
     return <div className={styles.output + " " + styles.repl}>
