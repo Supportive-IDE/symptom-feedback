0a75c0f6cea713fee5be098438cb7057d1753f0d
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-01-20 16:51:27 +0000

    fixed feedback for functionCallsNoParentheses and LocalVariablesAreGlobal

--- src/app/feedback/functionCallsNoParentheses.js
+++ src/app/feedback/functionCallsNoParentheses.js
@@ -17,19 +17,9 @@ export default function FunctionCallsNoParentheses({misconInfo}) {
 
     return <>
         <h1>Add <CodeInline code="()" /> after <CodeInline code={funcName} /> to call the function</h1>
-        {
-            undefinedVarOnLine !== -1 ?
-                <p><CodeInline code={funcName} /> is used like a variable on line {lineNumber} but it has not been 
-                initialised with a value. There is a {funcType === "userDefinedFunction" ? "function in the file" : "Python function"} with the same name. To call that function, add parentheses 
-                after the name and pass any expected arguments. If <CodeInline code={funcName} /> is intended to 
-                be a variable, it will need to be given a value before it is called.</p>
-            :
-                <p><CodeInline code={funcName} /> is used like a variable on line {lineNumber} but there is 
-                    no variable with this name in the file. There is a {funcType === "userDefinedFunction" ? "function in the file" : "Python function"} with the same name. To call that function, add parentheses 
-                    after the name and pass any expected arguments. If <CodeInline code={funcName} /> is intended to 
-                    be a variable, it will need to be given a value before it is called.</p>
-        }
-        <p>Note: In Python, functions are objects, which means they can be passed as arguments or assigned to variables like any other value. 
+        <p><CodeInline code={funcName} /> is used on line {lineNumber} without parentheses. If you meant to call the {funcType === "userDefinedFunction" ? "function" : "Python function"} with the same name, 
+        add <CodeInline code="()" /> after the name and pass any expected arguments.</p>
+        <p>Note: In Python, functions can be passed as arguments or assigned to variables like any other value. 
             If this was the intention on line {lineNumber}, then leave the code as it is.
         </p>
         <h2>An example</h2>
@@ -40,7 +30,7 @@ export default function FunctionCallsNoParentheses({misconInfo}) {
                     <MiniIDE startingCode={['def squared(x):', '\treturn x * x', '', 'num = int(input("Enter a whole number: "))', 'result = squared', 'print(num, "squared is", result)']} />
                     <p>In the last line of output, notice that the <CodeInline code="result" /> variable&apos;s value is <CodeInline code="<function squared>" />. This is because the programmer has forgotten to include 
                     the parentheses and argument when calling <CodeInline code="squared" /> on line 5. Because of this, the function is not called but is assigned to the variable <CodeInline code="result" />.</p>
-                    <p>Here is the corrected code. Spot the difference on line 5.</p>
+                    <p>Here is the corrected code. Line 5 has changed.</p>
                     <MiniIDE startingCode={['def squared(x):', '\treturn x * x', '', 'num = int(input("Enter a whole number: "))', 'result = squared(num)', 'print(num, "squared is", result)']} />
                 </>
                 :
@@ -48,7 +38,7 @@ export default function FunctionCallsNoParentheses({misconInfo}) {
                     <MiniIDE startingCode={['name = input', 'print("Hello, ", name)']} />
                     <p>In the last line of output, notice that the <CodeInline code="name" /> variable&apos;s value is <CodeInline code="<built-in function input>" />. This is because the programmer has forgotten to include 
                     the parentheses and argument when calling <CodeInline code="input" /> on line 1. Because of this, the function is not called but is assigned to the variable <CodeInline code="name" />.</p>
-                    <p>Here is the corrected code. Spot the difference on line 1.</p>
+                    <p>Here is the corrected code. Line 1 has changed.</p>
                     <MiniIDE startingCode={['name = input("What is your name? ")', 'print("Hello, ", name)']} />
                 </>
         }
--- src/app/feedback/localVariablesAreGlobal.js
+++ src/app/feedback/localVariablesAreGlobal.js
@@ -3,18 +3,64 @@ import CodeInline from "../codeInline";
 import { LINE_NUMBER } from "../config";
 import MiniIDE from "../miniIDE";
 
+
 export default function LocalVariablesAreGlobal({misconInfo}) {
     const lineNumber = misconInfo.has(LINE_NUMBER) ? Number(misconInfo.get(LINE_NUMBER)) : -1;
+    const localScopes = misconInfo.has("localScope") ? misconInfo.getAll("localScope") : [];
+    const localFunctions = misconInfo.has("localFunction") ? misconInfo.getAll("localFunction") : [];
     const varName = findAndConvertUrlParam(misconInfo, "text");
+
+
+    const explainCustomScopeIssue = () => {
+        if (localFunctions.length === 1 && localFunctions[0] !== "NA") {
+            return <>
+                <h2>Why doesn&apos;t <CodeInline code={varName} /> on line {lineNumber} get it&apos;s value from <CodeInline code={varName} /> in <CodeInline code={localFunctions[0] + "()"} />?</h2>
+                <p>The answer is <strong>scope</strong>. Variables defined in functions have <strong>local scope</strong>. This means 
+                they only exist in the function where they were defined.</p>
+                <p>In your code, <CodeInline code={varName} /> in <CodeInline code={localFunctions[0] + "()"} /> is <strong>local</strong> to 
+                the function <CodeInline code={localFunctions[0] + "()"} />.</p>
+                <p>The code on line {lineNumber} is in <strong>global</strong> scope. Code in global scope is outside of a function and cannot access code in local scope. Python 
+                considers <CodeInline code={varName} /> on line {lineNumber} to be a completely different variable from <CodeInline code={varName} /> in <CodeInline code={localFunctions[0] + "()"}/>.</p>
+                <p>The examples below provide hints for how to fix scope issues.</p>
+            </>
+        }
+        else if (localFunctions.length === 1 && localFunctions[0] === "NA") {
+            return <>
+                <h2>But there is already a variable called <CodeInline code={varName} /> defined in my code...</h2>
+                <p>Python views <CodeInline code={varName} /> on line {lineNumber} as different to the other <CodeInline code={varName} /> because of <strong>scope</strong>. Variables defined in functions 
+                have <strong>local scope</strong>. This means they only exist in the function where they were defined.</p>
+                <p>In your code, line {lineNumber} is in <strong>global</strong> scope. Code in global scope is outside of a function and cannot access code in local scope. Python 
+                considers <CodeInline code={varName} /> on line {lineNumber} to be a completely different variable from <CodeInline code={varName} /> in local scope.</p>
+                <p>The examples below provide hints for how to fix scope issues.</p>
+            </>
+        }
+        else if (localFunctions.length > 1) {
+            return <>
+                <h2>But there is already a variable called <CodeInline code={varName} /> defined in my code...</h2>
+                <p>Python views <CodeInline code={varName} /> on line {lineNumber} as different to the other <CodeInline code={varName} /> variables because of <strong>scope</strong>. Variables defined in functions 
+                have <strong>local scope</strong>. This means they only exist in the function where they were defined.</p>
+                <p>In your code, line {lineNumber} is in <strong>global</strong> scope. Code in global scope is outside of a function and cannot access code in local scope. Python 
+                considers <CodeInline code={varName} /> on line {lineNumber} to be a completely different variable from <CodeInline code={varName} /> in local scope.</p>
+                <p>The examples below provide hints for how to fix scope issues.</p>
+            </>
+        }
+        return <>
+            <h2>Could you have a scope issue?</h2>
+            <p>Variables defined in functions have <strong>local scope</strong>. This means 
+                they only exist in the function where they were defined.</p>
+            <p>In your code, there is another <CodeInline code={varName} /> variable defined in a function. Variables defined in functions 
+            have <strong>local scope</strong>. This means they only exist in the function where they were defined.</p>
+                <p>The code on line {lineNumber} is in <strong>global</strong> scope. Code in global scope is outside of a function and cannot access code in local scope. Python 
+                considers <CodeInline code={varName} /> on line {lineNumber} to be a completely different variable from <CodeInline code={varName} /> in local scope.</p>
+                <p>The examples below provide hints for how to fix scope issues.</p>
+        </>
+    }
+
 
     return <>
         <h1><CodeInline code={varName} /> on line {lineNumber} is undefined</h1>
-        <p><CodeInline code={varName} /> on line {lineNumber} is in global scope, where it has not been 
-        assigned a value. There is a variable with the same name in a function in this file. That variable is 
-        not accessible outside of the function. </p>
-        <p>If this error is caused by a typo, it can be fixed by assigning <CodeInline code={varName} /> a value 
-        before calling it on line {lineNumber}.</p>
-        <p>If the intention is to call the variable that is in function scope, it will need to be returned from the function.</p>
+        <p>Did you forget to assign <CodeInline code={varName} /> a value? If so, you can fix this error by assigning a value to <CodeInline code={varName} /> before using it on line {lineNumber}.</p>
+        { explainCustomScopeIssue() }
         <h2>Examples</h2>
         <p>Run the code below and take note of the error message:</p>
         <MiniIDE startingCode={['def squared(x):', '\treturn x * x', '', 'print(squared(x))']} />
