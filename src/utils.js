a994acdf109c6208d1d5d866ea78e08186231da3
Abi Evans <50892754+ace01603@users.noreply.github.com>
2025-03-03 08:18:07 +0000

    prevent repeat attempts at logging if it fails once

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { LOGGING_URL } from "./app/config";
 
+let canLog = true;
+
 export const findAndConvertUrlParam = (searchParams, paramName) => getParamValue(paramName, "", searchParams); // searchParams.has(paramName) ? decodeURIComponent(searchParams.get(paramName)) : "";
 
 export const getParamValue = (paramName, replacement, urlParams) => {
@@ -11,9 +13,13 @@ export const getParamValue = (paramName, replacement, urlParams) => {
 }
 
 export const sendData = async (data) => {
-    try {
-        await axios.put(`${LOGGING_URL}/extended`, {...data, urlParams: window.location.search});
-    } catch (error) {
-        console.log("Couldn't log", error);
+    if (canLog) {
+        try {
+            await axios.put(`${LOGGING_URL}/extended`, {...data, urlParams: window.location.search});
+        } catch (error) {
+            console.log("Couldn't log", error);
+            // disable future attempts
+            canLog = false;
+        }
     }
 }
